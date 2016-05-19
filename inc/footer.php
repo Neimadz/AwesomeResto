94475f394a649ab839201244f904e21d372d5dca
Anastasia Oudin Nikokosheva <a.nikokosheva@gmail.com>
2016-05-19 11:25:06 +0200

    jquery is included

--- inc/footer.php
+++ inc/footer.php
@@ -1,4 +1,5 @@
         </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
         <script src="../js/bootstrap.min.js"></script>
     </body>
 </html>
