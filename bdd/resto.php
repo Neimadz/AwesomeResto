43d245730a61ed1bf0dd0640069316b40d1156ba
Baptiste <baptistecousin78@gmail.com>
2016-05-19 12:05:43 +0200

    recette 1
    entrée    Griddled vegetables & feta with tabbouleh

--- bdd/resto.php
+++ bdd/resto.php
@@ -30,5 +30,71 @@ if($sql === false){
 	die(var_dump($bdd->errorInfo()));
 }
 
-$sql = $db->exec("INSERT INTO `recipes` (`id`, `role`, `title`, `content`, `link`, `ingredients`, `date_publish`) VALUES (NULL, 'entrance', 'zaeuio', 'azeiop', 'azeuio', 'azeuio', '2016-05-05 00:00:00')
+// Entrée 1    Griddled vegetables & feta with tabbouleh     recette 1
+
+$sql = $db->exec("INSERT INTO `recipes` (`id`, `role`, `title`, `content`, `link`, `ingredients`, `date_publish`) 
+	VALUES (NULL,
+	'entrance',
+	'Griddled vegetables & feta with tabbouleh',
+	'“This is a great meat-free recipe. Barbecuing a whole block of feta is a really interesting way to use it – you get a wicked texture contrast between the beautifully golden outside and the soft, creamy centre. The smoky flavour adds a subtle but beautiful twist to this delicious summery dish. ”
+	'http://cdn.jamieoliver.com/recipe-database/xtra_med/BWh5WOT0420A1wV_XQz54o.jpg',
+	'½	a bunch of	fresh oregano
+½	a bunch of	fresh flat-leaf parsley
+2	red onions
+1	large	aubergine
+150	g	feta cheese
+olive oil
+3	courgettes (a mixture of yellow and green)
+2	handfuls of	mixed tomatoes
+1	bulb of	garlic
+50	g	shelled pistachios
+2	tablespoons	runny honey
+extra virgin olive oil
+TABBOULEH
+250	g	cracked wheat
+1	bunch of	fresh mint
+1	big bunch of	fresh flat-leaf parsley
+½	a	cucumber
+1	lemon
+	'2016-19-05 11:20:5000')
+");
+
+$sql = $db->exec("INSERT INTO `recipes` (`id`, `role`, `title`, `content`, `link`, `ingredients`, `date_publish`) 
+	VALUES (NULL,
+	'entrance',
+	'Griddled vegetables & feta with tabbouleh',
+	'',
+	'',
+	'',
+	'2016-19-05 11:20:5000')
+");
+
+$sql = $db->exec("INSERT INTO `recipes` (`id`, `role`, `title`, `content`, `link`, `ingredients`, `date_publish`) 
+	VALUES (NULL,
+	'entrance',
+	'Griddled vegetables & feta with tabbouleh',
+	'',
+	'',
+	'',
+	'2016-19-05 11:20:5000')
+");
+
+$sql = $db->exec("INSERT INTO `recipes` (`id`, `role`, `title`, `content`, `link`, `ingredients`, `date_publish`) 
+	VALUES (NULL,
+	'entrance',
+	'Griddled vegetables & feta with tabbouleh',
+	'',
+	'',
+	'',
+	'2016-19-05 11:20:5000')
+");
+
+$sql = $db->exec("INSERT INTO `recipes` (`id`, `role`, `title`, `content`, `link`, `ingredients`, `date_publish`) 
+	VALUES (NULL,
+	'entrance',
+	'Griddled vegetables & feta with tabbouleh',
+	'',
+	'',
+	'',
+	'2016-19-05 11:20:5000')
 ");
