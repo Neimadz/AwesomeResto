cb3efe0cc50c1a8b37e96ca33829ab25c9c4eb11
AbrakaDams <damien.machado33@gmail.com>
2016-05-19 14:36:28 +0200

    bdd update x0.4

--- bdd/resto.php
+++ bdd/resto.php
@@ -125,24 +125,157 @@ a few	shoots from winter cabbages, such as kale or cavolo nero , optional
 	','2016-19-05 11:20:5000')
 ");
 
-
-
-$sql = $db->exec("INSERT INTO `recipes` (`id`, `role`, `title`, `content`, `link`, `ingredients`, `date_publish`)
+// Plat 1        Piri piri chicken    recette 4
+$sql = $db->exec("INSERT INTO `recipes` (`id`, `role`, `title`, `content`, `link`, `ingredients`, `date_publish`) 
 	VALUES (NULL,
-	'entrance',
-	'Griddled vegetables & feta with tabbouleh',
-	'',
-	'',
-	'',
-	'2016-19-05 11:20:5000')
+	'dish',
+	'Piri piri chicken',
+	'“Crisp, spicy roast chicken, served with piri piri sauce, jalapeño salsa and sweet potato wedges – delicious! ”',
+	'http://cdn.jamieoliver.com/recipe-database/oldImages/xtra_med/1418_1_1436873314.jpg',
+	'1.3	kg	free-range chicken
+3	sprigs of	fresh thyme
+4	cloves of	garlic
+1	teaspoon	sweet smoked paprika
+sea salt
+freshly ground black pepper
+olive oil
+2	red onions
+4	ripe tomatoes	, mixed colours if possible
+6	fresh chillies	, mixed colours if possible
+red wine vinegar
+extra virgin olive oil
+For the sweet potato wedges:
+750	g	sweet potatoes
+1	teaspoon	smoked paprika
+2	tablespoons	fine semolina
+For the jalapeño salsa:
+1 x 200	g jar of	pickled jalapeños
+1	bunch of fresh coriander',
+	'2016-08-04 15:25:5033')
 ");
 
-$sql = $db->exec("INSERT INTO `recipes` (`id`, `role`, `title`, `content`, `link`, `ingredients`, `date_publish`)
+
+// Plat 2   Wild boar burgers    recette 5
+$sql = $db->exec("INSERT INTO `recipes` (`id`, `role`, `title`, `content`, `link`, `ingredients`, `date_publish`) 
 	VALUES (NULL,
-	'entrance',
-	'Griddled vegetables & feta with tabbouleh',
-	'',
-	'',
-	'',
-	'2016-19-05 11:20:5000')
+	'dish',
+	'Wild boar burgers',
+	'“These might be the best boar burgers ever! ”',
+	'http://cdn.jamieoliver.com/recipe-database/oldImages/xtra_med/1140_1_1436963553.jpg',
+	'500	g	minced wild boar shoulder	, or belly
+1	tbsp	brown sauce	, or to taste
+Worcestershire sauce	, to taste
+1	tbsp	tomato sauce	, or to taste
+2	shallots	, finely chopped
+4	burger buns
+olive oil
+2	braeburn apples	, or cox\'s apples, sliced at the last minute to serve',
+	'2016-25-02 18:20:5240')
 ");
+
+// Plat 3   Marmexican marinated pork tenderloin    recette 6
+$sql = $db->exec("INSERT INTO `recipes` (`id`, `role`, `title`, `content`, `link`, `ingredients`, `date_publish`) 
+	VALUES (NULL,
+	'dish',
+	'Marmexican marinated pork tenderloin',
+	'“A great Friday night recipe with friends. Get everything in the middle of the table and tuck in.”',
+	'http://cdn.jamieoliver.com/recipe-database/xtra_med/74B9VPWGqs8BGNa_ee3BCy.jpg',
+	'500	g	piece of pork tenderloin	, trimmed
+Olive oil
+18	small	corn tortillas
+lime wedges	, to serve
+Marinade:
+Olive oil
+2	red onions	, peeled and chopped
+6	cloves of	garlic	, peeled and chopped
+1/2	large bunch of	coriander	, stalks only (use the leaves for the salsa), finely chopped
+2	dried chipotle chillies	, soaked in hot water for 10 minutes, drained, then chopped
+1	tsp	ground cumin
+1/2	tsp	sweet smoked paprika
+3	sprigs of	thyme
+2	tbsp	red wine vinegar
+1	tbsp	tomato purée
+2	tbsp	dark brown sugar
+1/2	orange , zest and juice of
+Rainbow salad:
+1	corn on the cob	, soaked for 10 minutes in cold water
+1/4	white cabbage	, finely shredded
+2	tbsp	white vinegar
+6	mixed radishes	, thinly sliced
+2	carrots	, speed-peeled into ribbons with a potato peeler
+3	little gem hearts	, cut into thin wedges
+Avocado salsa:
+2	ripe avocados	, flesh of, smashed
+1	green chilli	, finely chopped (deseeded if you like)
+2	spring onions	, finely chopped
+1/2	small bunch of	coriander	, leaves picked, roughly chopped
+1	lime	, zest and juice of
+Extra virgin olive oil',
+	'2016-29-03 16:30:4010')
+");
+
+// Dessert 1   Epic ice cream cake   recette 7
+$sql = $db->exec("INSERT INTO `recipes` (`id`, `role`, `title`, `content`, `link`, `ingredients`, `date_publish`) 
+	VALUES (NULL,
+	'dessert',
+	'Epic ice cream cake',
+	'“Choose two or three different flavours of your favourite ice cream for this cake – we went for pistachio, strawberry and vanilla. Then when you slice it open all of the amazing layers will be revealed. ”',
+	'http://cdn.jamieoliver.com/recipe-database/xtra_med/8dhIZVN1qOS9cBVMAjX98l.jpg',
+	'90	g	butter	, at room temperature, plus extra for greasing
+90	g	light brown sugar
+1	large	free-range egg
+30	g	ground almonds
+60	g	self-raising flour
+1	tablespoon	cocoa powder
+¼	heaped teaspoon	baking powder
+30	g	dark chocolate (70% cocoa solids)
+50	ml	milk
+1.5	litres	ice cream (2-3 flavours)',
+	'2016-28-01 08:10:4440')
+");
+
+// Dessert 2    Chocolate battenberg  recette 8
+$sql = $db->exec("INSERT INTO `recipes` (`id`, `role`, `title`, `content`, `link`, `ingredients`, `date_publish`) 
+	VALUES (NULL,
+	'dessert',
+	'Chocolate battenberg',
+	'“This is made with cocoa instead of pink food colouring. Find marzipan in the supermarket baking aisle. Use the cake off-cuts in a trifle. ”',
+	'http://cdn.jamieoliver.com/recipe-database/xtra_med/6H0HUbu6q3v91gE5eklQf6.jpg',
+	'350	g	butter	, at room temperature, plus extra for greasing
+350	g	caster sugar
+6	large	free-range eggs
+½	tablespoon	vanilla extract
+2	tablespoons	milk
+285	g	self-raising flour
+50	g	ground almonds
+40	g	cocoa powder
+125	g	raspberry jam
+450	g	marzipan or almond paste',
+	'2015-14-12 18:20:5540')
+");
+
+// Dessert 3    Chocolate & salted caramel cake  recette 9
+$sql = $db->exec("INSERT INTO `recipes` (`id`, `role`, `title`, `content`, `link`, `ingredients`, `date_publish`) 
+	VALUES (NULL,
+	'dessert',
+	'Chocolate & salted caramel cake',
+	'“You don’t use all the caramel, but it keeps in the fridge for a few days. ”',
+	'http://cdn.jamieoliver.com/recipe-database/xtra_med/2kUz9apvqo49riq4lB5FBh.jpg',
+	'250	g	unsalted butter	, plus extra for greasing
+250	g	dark chocolate (70% cocoa solids)
+150	ml	espresso
+225	g	self-raising flour
+250	g	golden caster sugar
+250	g	soft light brown sugar
+2	tablespoons	cocoa powder
+100	ml	buttermilk
+4	large	free-range eggs
+SALTED CARAMEL
+200	g	sugar
+50	g	butter
+75	ml	double cream
+CHOCOLATE GANACHE
+50	g	dark chocolate (70% cocoa solids)
+50	ml	double cream',
+	'2015-12-08 11:20:5240')
+"); 
