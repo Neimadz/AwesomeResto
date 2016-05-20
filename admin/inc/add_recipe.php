fb4baef207d2a31530e65c40a3f9a9718a2dbd84
AbrakaDams <damien.machado33@gmail.com>
2016-05-20 18:14:56 +0200

    Update add recipe
    ajout de type file

--- admin/inc/add_recipe.php
+++ admin/inc/add_recipe.php
@@ -1,6 +1,109 @@
+<?php  
+include_once 'functions.php';
+require_once 'connect.php';
+
+$post = []; // Le tableau qui contiendra les données de mon formulaire nettoyées
+$error = []; // J'instancie mon tableau d'erreurs
+$maxSize = 500000; // Taille maxi de mes fichiers (en octets)
+$folder = '../img/'; // Dossier de destination de mes fichiers
+$possibleRole = ['entrance', 'dish', 'dessert']; // acceptable values
+
+if(!empty($_FILES) && isset($_FILES['image']) && $_FILES['image']['error'] == UPLOAD_ERR_OK && $_FILES['image']['size'] < $maxSize){
+    
+    $fileName = $_FILES['image']['name']; // Nom de mon image
+    $fileTemp = $_FILES['image']['tmp_name']; // Image temporaire
+
+    $file = new finfo(); // Classe FileInfo
+    $mimeType = $file->file($_FILES['image']['tmp_name'], FILEINFO_MIME_TYPE); // Retourne le VRAI mimeType
+    $mimeTypeAllowed = ['image/jpg', 'image/jpeg', 'image/png','image/gif']; // Les mime types autorisés
+
+    // Permet de vérifier que le mime type est bien autorisé
+    if(in_array($mimeType, $mimeTypeAllowed)){
+        /*
+         * explode() permet de séparer une chaine de caractère en un tableau
+         * Ici, on aura donc : 
+         *      $newFileName = array(
+                    0 => 'nom-de-mon-fichier', 
+                    1 => 'jpg'
+                );
+         */
+        $newFileName = explode('.', $fileName);
+        $fileExtension = end($newFileName); // Récupère l'extension du fichier
+
+        $finalFileName = 'user-'.time().$fileExtension; // Le nom du fichier sera donc : user-1463058435.jpg (time() retourne un timestamp à la seconde). Cela permet de sécuriser l'upload de fichier
+
+
+        if(move_uploaded_file($fileTemp, $folder.$finalFileName)){
+            // Ici je suis sur que mon image est au bon endroit
+            $imageFinale = $folder.$finalFileName;
+        }
+        else {
+            $imageFinale = 'img/default-avatar.jpg'; // Permet d'avoir une image par défaut si l'upload ne s'est pas bien déroulé... Evidemment, il faut que l'image existe :-)
+        }
+    }
+    else {
+        $error[] = 'Le mime type est interdit';
+    }
+
+}
+else {
+    $error[] = 'L\'image est trop lourde';
+}
+
+if(!empty($_POST)){ // On vérifie que le formulaire est soumis
+
+    foreach($_POST as $key => $value){ // On nettoie un peu :-)
+        $post[$key] = trim(strip_tags($value));
+    }
+    if(verif(("#^.{5,140}$#"), $post['title'])){
+      $errors[] = 'Le titre de la recette doit comporter entre 5 et 140 cractères';
+    }
+    if(verif(("#^.{20,}$#"), $post['content'])){
+      $errors[] = 'Le contenu doit faire au moins 20 caractères';
+    }
+    if(empty($post['ingredients'])) {
+      $errors[] = 'La listes d\'ingrédients doit être complète';
+    }
+    if(empty($post['role']) || !in_array($post['role'], $possibleRole)) {
+      $errors['ok'] = 'Précisez la catégorie, svp';
+    }
+    if(count($error) > 0){
+        $showErrors = true;
+    }
+    else {
+        $requete = $db->prepare('INSERT INTO recipes (author_id, title, role, content, link, ingredients, date_publish) VALUES(:myId, :myTitle, :role, :myContent, :myLink, :myIngredients, NOW())');
+        $requete->bindValue(':myId', $_SESSION['user']['id']);
+        $requete->bindValue(':myTitle', $post['title']);
+        $requete->bindValue(':role', $post['role']);
+        $requete->bindValue(':myContent', $post['content']);
+        $requete->bindValue(':myLink', $imageFinale);
+        $requete->bindValue(':myIngredients', $post['ingredients']);
+
+        if($requete->execute()){ // Si la requete s'execute correctement
+            $success = true;
+        }
+
+    }
+}
+?>
+<?php 
+    if(isset($success) && $success == true){
+        echo '<p style="color:green">Well done !</p>';
+    }
+    if(isset($showErrors) && $showErrors == true){
+        // Afficher mes erreurs
+        echo '<ul style="color:red">';
+        foreach($error as $err){
+            echo '<li>'.$err.'</li>';
+        }
+        echo '</ul>';
+        // Moins sexy, mais fonctionnel :
+        //echo implode('<br>', $error);
+    }
+?>
 <h1>Ajouter une recette</h1>
 
-<form id="add-recipe-form" method="POST">
+<form id="add-recipe-form" enctype="multipart/form-data" method="POST">
     <div class="form-group">
         <label for="role">Catégorie :</label>
         <select name="role" id="add-role" class="form-control" size="1">
@@ -23,7 +126,8 @@
 
     <div class="form-group">
         <label for="link">Votre image : </label>
-        <input type="text" id="add-link" class="form-control" name="link"  placeholder="Insérez votre image ici">
+        <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $maxSize; ?>">
+        <input type="file" id="add-link" class="form-control" name="image"  placeholder="Insérez votre image ici">
     </div>
 
     <div class="form-group">
--- admin/inc/add_recipe_treat.php
+++ admin/inc/add_recipe_treat.php
@@ -1,12 +1,58 @@
 <?php
 session_start();
 require_once 'connect.php'; // dosn't work without
+include_once 'functions.php';
 
 $post = array();
 $error = array();
 $displayErr = false; // show errors
 $formValid = false; // form was filled well and data was inserted to the db
+$maxSize = 500000; // Taille maxi de mes fichiers (en octets)
+$folder = '../img/'; // Dossier de destination de mes fichiers
 $possibleRole = ['entrance', 'dish', 'dessert']; // acceptable values
+
+if(!empty($_FILES) && isset($_FILES['image']) && $_FILES['image']['error'] == UPLOAD_ERR_OK && $_FILES['image']['size'] < $maxSize){
+    
+    $fileName = $_FILES['image']['name']; // Nom de mon image
+    $fileTemp = $_FILES['image']['tmp_name']; // Image temporaire
+
+    $file = new finfo(); // Classe FileInfo
+    $mimeType = $file->file($_FILES['image']['tmp_name'], FILEINFO_MIME_TYPE); // Retourne le VRAI mimeType
+    $mimeTypeAllowed = ['image/jpg', 'image/jpeg', 'image/png','image/gif']; // Les mime types autorisés
+
+    // Permet de vérifier que le mime type est bien autorisé
+    if(in_array($mimeType, $mimeTypeAllowed)){
+        /*
+         * explode() permet de séparer une chaine de caractère en un tableau
+         * Ici, on aura donc : 
+         *      $newFileName = array(
+                    0 => 'nom-de-mon-fichier', 
+                    1 => 'jpg'
+                );
+         */
+        $newFileName = explode('.', $fileName);
+        $fileExtension = end($newFileName); // Récupère l'extension du fichier
+
+        $finalFileName = 'user-'.time().$fileExtension; // Le nom du fichier sera donc : user-1463058435.jpg (time() retourne un timestamp à la seconde). Cela permet de sécuriser l'upload de fichier
+
+
+        if(move_uploaded_file($fileTemp, $folder.$finalFileName)){
+            // Ici je suis sur que mon image est au bon endroit
+            $imageFinale = $folder.$finalFileName;
+        }
+        else {
+            $imageFinale = 'img/default-avatar.jpg'; // Permet d'avoir une image par défaut si l'upload ne s'est pas bien déroulé... Evidemment, il faut que l'image existe :-)
+        }
+    }
+    else {
+        $error[] = 'Le mime type est interdit';
+    }
+
+}
+else {
+    $error[] = 'L\'image est trop lourde';
+}
+
 
 if(!empty($_POST)) {
     // clean user input
@@ -14,14 +60,11 @@ if(!empty($_POST)) {
       $post[$key] = trim(strip_tags($value));
     }
     // verification
-    if(strlen($post['title']) < 5 || strlen($post['title']) > 200) {
-      $error[] = 'Le titre de la recette doit comporter entre 5 et 200 cractères';
+    if(verif(("#^.{5,140}$#"), $post['title'])){
+      $errors[] = 'Le titre de la recette doit comporter entre 5 et 140 cractères';
     }
-    if(empty($post['content'])) {
-      $error[] = 'Le contenu ne peut être vide';
-    }
-    if(!filter_var($post['link'], FILTER_VALIDATE_URL)) {
-      $error[] = 'Le lien de l\'image n\'est pas valide';
+    if(verif(("#^.{20,}$#"), $post['content'])){
+      $errors[] = 'Le contenu doit faire au moins 20 caractères';
     }
     if(empty($post['ingredients'])) {
       $error[] = 'La listes d\'ingrédients doit être complète';
@@ -46,7 +89,7 @@ if(!empty($_POST)) {
         $sql->bindValue(':roleRecipe', $post['role']);
         $sql->bindValue(':titleRecipe', $post['title']);
         $sql->bindValue(':contentRecipe', $post['content']);
-        $sql->bindValue(':linkRecipe', $post['link']);
+        $sql->bindValue(':linkRecipe', $imageFinale);
         $sql->bindValue(':ingredientsRecipe', $post['ingredients']);
         $sql->bindValue(':authorId', $authorId);
 
