a599f5d7f7b5f2c3d9186e9ffb57fec1ee5ccd17
Millaa Millaa <contact.myriambugnazet@gmail.com>
2016-05-20 18:36:49 +0200

    lost password suite

--- admin/lost_password.php
+++ admin/lost_password.php
@@ -10,149 +10,161 @@ $post = [];
 
 $showFormEmail = true;    // On affiche le 1er formulaire de saisie de l email
 $showFormPassword = false; // On affiche le 2nd formulaire de mise à jour de notre mdp
-
-//var_dump($_POST);
-// On masque le 1er formulaire si token et email dans le GET pour afficher le 2nd
+ 
 if(isset($_GET['token']) && !empty($_GET['token']) && isset($_POST['email']) && !empty($_POST['email'])) {
 	$showFormEmail = false;   
 	$showFormPassword = true;
 }
+
 // Traitement des formulaires 
 if(!empty($_POST)) {
-	// Nettoyage des données
-	foreach($_POST as $k => $v) {
-		$post[$k] = $v;
+// Nettoyage des données
+	foreach($_POST as $key => $value) {
+		$post[$key] = trim(strip_tags($value));
 	}
+
 // Traitement du formulaire du mail
-	if(isset($post['action']) && $post['action'] == 'generateToken') {
-		if(filter_var($post['email_password'], FILTER_VALIDATE_EMAIL)) {
-			$sql = $db->prepare('SELECT email from users WHERE email= :email');
-			$sql->bindValue(':email', $post['email_password']);
-			$sql->execute();
-			$ifMailExist = $sql->fetchColumn();
+if(isset($post['action']) && $post['action'] == 'generateToken') {
+var_dump($post);
+	if(filter_var($post['email_password'], FILTER_VALIDATE_EMAIL)) {
+		$req = $db->prepare('SELECT email from users WHERE email= :email');
+		$req->bindValue(':email', $post['email_password']);
+		$req->execute();
 
-			if(!empty($ifMailExist)) {    // On search une corres avec le mail
-				$token = md5(uniqid()); // Création du token
+		$emailExist = $req->fetchColumn();
 
-				$insert = $db->prepare('INSERT INTO tokens_password (email, token, date_create, date_exp) VALUES (
-					:emailInsert,
-					:tokenInsert,
-					NOW(),
-					(NOW() + INTERVAL 2 DAYS))
-					');
-				$insert->bindValue(':emailInsert', $post['email_password']);
-				$insert->bindValue(':tokenInsert', $token);
-				if($insert->execute()) {
-					
-		        $mail = new PHPMAILER;
-		        $mail->isSMTP();                                      
-                $mail->Host = 'smtp.mailgun.org';  
-                $mail->SMTPAuth = true;                               
-                $mail->Username = 'postmaster@wf3.axw.ovh';                 
-                $mail->Password = 'WF3sessionPhilo2';                           
-                $mail->SMTPSecure = 'tls';                           
-                $mail->Port = 587;                                   
+		if(!empty($emailExist)) {    // On search une corres avec le mail
 
-                $mail->setFrom($post['email'], $post['firstname'] );
-                $mail->addAddress('baptistecousin78@gmail.com', ' Anastasia Admin'); 
-        
-                $mail->isHTML(true);                                  
+			$token = md5(uniqid()); // Création du token
 
-                $mail->Subject = 'Voici un lien pour générer un nouveau mot de passe : generate_new_password.php';
-                $mail->Body    = $token;
-                $mail->AltBody = $token;
+			$insert = $db->prepare('INSERT INTO tokens_password (email, token, date_create, date_exp) VALUES (
+				:emailInsert,
+				:tokenInsert,
+				NOW(),
+				(NOW() + INTERVAL 2 DAYS))
+				');
+			$insert->bindValue(':emailInsert', $post['email_password']);
+			$insert->bindValue(':tokenInsert', $token);
+			if($insert->execute()) {
 
-        			if(!$mail->send()) {
-            			echo 'Le message ne peut être envoyé.';
-           				 echo 'Mailer Error: ' . $mail->ErrorInfo;
-        				} else {
-           				 echo 'Le message a bien été envoyé et nous vous remercions';
-        				}		
-					}
-			}		
+		       	$mail = new PHPMAILER;
+		        //$mail->SMTPDebug = 3;                               // Enable verbose debug output
+		        $mail->isSMTP();                                      // Set mailer to use SMTP
+	        	$mail->Host = 'smtp.mailgun.org'; // Specify main and backup SMTP servers
+	        	$mail->SMTPAuth = true;                               // Enable SMTP authentication
+	        	$mail->Username = 'postmaster@wf3.axw.ovh';                 // SMTP username
+	        	$mail->Password = 'WF3sessionPhilo2';                           // SMTP password
+	        	$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+	        	$mail->Port = 587;                                    // TCP port to connect to
+
+	        	$mail->setFrom('contact@monsupersite.fr', 'contact du site'); //expéditeur
+	        	$mail->addAddress('contact.myriambugnazet@gmail.com', 'millaa');  // Add a recipient// Name is optional
+	        	$mail->addReplyTo('info@example.com', 'Information');// si on l'enlève ça renvoie auto à l'expéditeur
+
+	       	 	$mail->isHTML(true);                                  // Set email format to HTML
+
+	        	$mail->Subject = 'Here is the subject';
+	        	$mail->Body    = $post['message'];
+	        	$mail->AltBody = $post['message'];
+			}//fin if insert execute
+    			
+    		if(!$mail->send()) {
+        		echo 'Le message ne peut être envoyé.';
+       			echo 'Mailer Error: ' . $mail->ErrorInfo;
+    		} else {
+       			echo 'Le message a bien été envoyé et nous vous remercions';	
+			}
+
+		}//if empty emailexist
+	}//fin filter var
+
+		else 
+		{
+		$error[] = 'Votre adresse email est incorrecte';
 		}
-	}
+}// fin if EMPTYpost
 
-
-// Traitement du 2nd formulaire concernant la maj du mdp
+	// Traitement du 2nd formulaire concernant la maj du mdp
 	elseif(isset($post['action']) && $post['action'] == 'updatePassword') {
+
 		if(strlen($post['new_password']) < 8 || strlen($post['new_password']) > 25 ) { // Nbres de caractères modifiables 
 			$error[] = 'Votre mot de passe doit contenir entre 8 et 25 caractères';
 		}
+
 		if($post['new_password'] != $post['new_password_conf']) {
 			$error[] = 'Vos mots de passe doivent correspondre';
 		}
+
 		if(count($error) == 0 ) { // On compte nos erreurs, vérif du token et du mail
-			$token = $db->prepare('SELECT * FROM tokens_password WHERE email = :postEmail AND token = :postToken AND date_exp < NOW()');
-			$token->bindValue(':postEmail', $post['email']);
-			$token->bindValue(':postToken', $post['token']);
-			$token->execute();
+			$token = $db->prepare('SELECT * FROM tokens_password WHERE email = :email AND token = :token AND date_exp < NOW()');
+		$token->bindValue(':email', $post['email']);
+		$token->bindValue(':token', $post['token']);
+		$token->execute();
 
-			$tokenExist = $token->fetch();
+		$tokenExist = $token->fetch();
 
-			if(empty($tokenExist)) {
-				$error[] = 'Il ya une erreur !';
-			} 
-			else {
-				// Changement du mdp
-				$update = $db->prepare('UPDATE users SET password = :newPassword WHERE email = :email');
-				$update->bindValue(':newPassword', password_hash($post['new_password'], PASSWORD_DEFAULT));
-				$update->bindValue(':email', $post['email']);
-				if($update->execute()) {
-					$successUpdate = true;
+		if(empty($tokenExist)) {
+			$error[] = ' Le token et l\'adresse email ne correspondent pas. !';
+		} 
+		else {
+			// Ici, on peut ENFIN changer ce putain de mot de passe :-)
+			$update = $db->prepare('UPDATE users SET password = :password WHERE email = :email');
+			$update->bindValue(':password', password_hash($post['new_password'], PASSWORD_DEFAULT)); // On insère le mot de passe hashé
+			$update->bindValue(':email', $post['email']);
+			if($update->execute()) {
+				$successUpdate = true;
 
-                // Suppression du token car le mdp est modifié
-					$delete = $db->prepare('DELETE FROM tokens_password WHERE id = :idToken');
-					$delete->bindValue(':idToken', $tokenExist['id'], PDO::PARAM_int).
-					$delete->execute();
-				}
+	        // Suppression du token car le mdp est modifié
+				$delete = $db->prepare('DELETE FROM tokens_password WHERE id = :idToken');
+				$delete->bindValue(':idToken', $tokenExist['id'], PDO::PARAM_int);// $tokenExist contient les infos de mon token extraites de la base de données.. et donc son ID 
+				$delete->execute();
+			}//fin if udpdate
+		} //fin else
+	}//fin if count
+	}//fin elsif
 
-			}
-		}
-	}
-
-}
+}//fin if isset
 ?>
 
- <main class="container">
- 	<h1 class="text-center">Mot de passe oublié ?</h1>
+<main class="container">
+<h1 class="text-center">Mot de passe oublié ?</h1>
+<br>
+<?php if(!empty($error)): // Affichage des erreurs si êrror n est pas vide ?>
+	<div class="alert alert-danger">
+	    <?=implode('<br>', $error); ?>
+	</div>
+<?php endif; ?>
+
+<?php if(isset($showFormEmail) && $showFormEmail == true): // Affichage du 1er form?>
+<?php if(isset($linkChangePassword)): // mail ok et token inséré?>	
+
+ 	<p>Vous pouvez réinitialiser votre mot de passe en cliquant sur le lien ci dessous : 
  	<br>
- 	<?php if(!empty($error)): // Affichage des erreurs si êrror n est pas vide ?>
- 		<div class="alert alert-danger">
- 		    <?=implode('<br>', $error); ?>
- 		</div>
- 	<?php endif; ?>
- 	
- 	<?php if(isset($showFormEmail) && $showFormEmail == true): // Affichage du 1er form?>
-	    <?php if(isset($linkChangePassword)): // mail ok et token inséré?>	
+ 	<a href="<?=$linkChangePassword; ?>">Modifier votre mot de passe</a>   <!-- Vérif si lien ok-->
+ 	</p>
+ 	<br>
 
-		 	<p>Vous pouvez réinitialiser votre mot de passe en cliquant sur le lien ci dessous : 
-		 	<br>
-		 	<a href="<?=$linkChangePassword; ?>">Modifier votre mot de passe</a>   <!-- Vérif si lien ok-->
-		 	</p>
-		 	<br>
+ 	<code><?=$linkChangePassword; ?></code>        <!-- Vérif si ok -->
 
-		 	<code><?=$linkChangePassword; ?></code>        <!-- Vérif si ok -->
+	<?php else: // Affichage du form ?>
+		<!-- Affichage du formulaire avec notre adresse mail -->
+ 	<form class="form-horizontal well-well-sm" method="post">
+ 	    <div class="form-group">
+ 	        <label class="col-md-4 control-label" for="email">Email : </label>
+ 	        <div class="col-md-4">
+ 	            <input id="email" type="email" name="email" placeholder="votre@gmail.com" class="form-control input-md" required>
+ 	        </div>
+ 	    </div>
 
- 		<?php else: // Affichage du form ?>
- 			<!-- Affichage du formulaire avec notre adresse mail -->
-		 	<form class="form-horizontal well-well-sm" method="post">
-		 	    <div class="form-group">
-		 	        <label class="col-md-4 control-label" for="email">Email : </label>
-		 	        <div class="col-md-4">
-		 	            <input id="email" type="email" name="email" placeholder="votre@gmail.com" class="form-control input-md" required>
-		 	        </div>
-		 	    </div>
+ 	    <div class="form-group">
+ 	        <div class="col-md-4 col-md-offset-4">
+ 	            <button type='submit' class="btn btn-primary">Envoyez moi un nouveau de passe !</button>
+ 	        </div>
+ 	    </div> 
+ 	</form> 
 
-		 	    <div class="form-group">
-		 	        <div class="col-md-4 col-md-offset-4">
-		 	            <button type='submit' class="btn btn-primary">Envoyez moi un nouveau de passe !</button>
-		 	        </div>
-		 	    </div> 
-		 	</form> 
-
-    	<?php endif; ?> 
- 	<?php endif; // Fermeture du ifelse de $showFormEmail ?>
+<?php endif; ?> 
+<?php endif; // Fermeture du ifelse de $showFormEmail ?>
 
  	<?php if(isset($showFormPassword) && $showFormPassword == true): ?>
 
@@ -181,6 +193,6 @@ if(!empty($_POST)) {
             </div>
         </form>
 
- 	<?php endif; ?>
+ 	<?php endif; ?><!-- fermeture isset showformpassword -->
 
 
