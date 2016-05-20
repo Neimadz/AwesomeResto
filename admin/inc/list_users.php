74bb6a09e6f9ba46076ad02b49d7d09bc5e9517c
Anastasia Oudin Nikokosheva <a.nikokosheva@gmail.com>
2016-05-20 13:36:53 +0200

    admin/inc/list_users.php is done

--- admin/inc/list_users.php
+++ admin/inc/list_users.php
@@ -4,9 +4,20 @@ $getUsers = $db->prepare('SELECT * FROM users');
 $getUsers->execute();
 $users = $getUsers->fetchAll(PDO::FETCH_ASSOC);
 if(!empty($users)) {
+    $count = 1;
     echo '<ul class="list-group">';
+    // loop that prints all the users
     foreach ($users as $key => $value) {
-        echo '<li class="list-group-item">'. $value['firstname']. '</li>';
+        echo '<li class="list-group-item">' .
+             $count . '. '                   .
+             $value['firstname']            .
+             ' <span class="label '         .
+             // condition to check $value role: if role is admin print label-success, else print label-primary
+             (($value['role'] == "admin")?"label-success":"label-primary") .
+             '">'                           .
+             $value['role']                 .
+             '</span></li>';
+             $count++;
     }
     echo '</ul>';
 }
