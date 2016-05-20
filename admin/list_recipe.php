8e4340994c342fabf356531bef7763398ad87c41
Noe <champignyn@gmail.com>
2016-05-20 11:55:21 +0200

    UPDATE de list, update et delete article + du css

--- admin/list_recipe.php
+++ admin/list_recipe.php
@@ -11,14 +11,18 @@ include_once 'inc/header.php';
 
 foreach($allRecipes as $plat => $recettes){
 	echo '<div class="row">';
-		echo '<h3>'.$recettes['title'].'</h3><br>';
+		echo '<h3 class="title-list">'.$recettes['title'].'</h3><br>';
 		echo '<img class="img-list col-sm-2" src="'.$recettes['link'].'">';
-		echo '<div class="col-sm-6">'.cutString($recettes['content'],0, 300, '...')."</div>";
+		echo '<div class="col-sm-6">';
+			echo cutString($recettes['content'],0, 220, ' [...]').'<br><br>';
+			echo '<p class="text-right"><strong>Ajouté le : </strong>'.$recettes['date_publish']."<strong> par : </strong><em>".$recettes['author_id'].'</em></p>';
+		echo '</div>';
 		echo '<div class="wrapper-buttons col-sm-4">';
-			echo '<a href=""><span class="glyphicon glyphicon-wrench"></span> Modifier</a><br>';
-			echo '<a href=""><span class="glyphicon glyphicon-remove"></span> Supprimer</a>';
+			echo '<a href="update_article.php?='.$recettes['id'].'"><span class="glyphicon glyphicon-wrench"></span> Modifier</a><br>';
+			echo '<a href="delete_article.php?='.$recettes['id'].'" onclick="return confirm(\'Êtes vous sur de vouloir supprimer cela ?\');"><span class="glyphicon glyphicon-remove"></span> Supprimer</a>';
 		echo '</div>';
 	echo '</div>';
 }
+var_dump($recettes);
 include_once 'inc/footer.php';
 ?>
