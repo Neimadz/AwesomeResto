6f7f8fa8816a3893807367f74f0b1f9c836503a6
Millaa Millaa <contact.myriambugnazet@gmail.com>
2016-05-22 21:38:48 +0200

    le css des recettes sur read_recipe
    une page une recette

--- read_recipe.php
+++ read_recipe.php
@@ -17,24 +17,25 @@ if(isset($_GET['id']) && !empty($_GET['id'])){
 if(!empty($_GET)){
 	if(isset($idRecipe)){
 	// Prépare et execute la requète SQL pour récuperer notre recipe de manière dynamique
-	$res = $db->prepare('SELECT * FROM recipe WHERE id = :idRecipe');
+	$res = $db->prepare('SELECT * FROM recipes WHERE id = :idRecipe');
 	$res->bindParam(':idRecipe', $idRecipe, PDO::PARAM_INT);
 	$res->execute();
 
 	// $recipe contient mon recipe extrait de la bdd
 	$recipe = $res->fetch(PDO::FETCH_ASSOC);
 
-	echo '<div class="img">';
+	echo '<div class="imgOneRecipe">';
 	echo '<br>';
-	echo '<h2>' .$recipe['title'] . '</h2>';
+	echo '<h2 class="readTitle">' .$recipe['title'] . '</h2>';
 	echo '<br>';
-	echo '<p>Publié le : ' .date('d/ m/ Y', strtotime($recipe['date_add']));
+	echo '<p class="readPublish">Publié le : '.$recipe['date_publish']; 
 	echo '<br><br>';
-	echo '<img class="imgonerecipe" src="'.$recipe['link'].'">';
+	echo '<img class="" src="'.$recipe['link'].'">';
 	echo '<br><br>';
-	echo '<p class="detailtxt">' .$recipe['content'].'</p>';
+	echo '<p class="readContent">' .$recipe['content'].'</p>';
 	echo '<br>';
 	echo '</div>';
+
 	} else {
 		echo 'Article introuvable !';
 	}
