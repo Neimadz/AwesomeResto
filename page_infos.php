75aec8d5a4edb65ce44e16acff208cb9a2424fc2
Baptiste <baptistecousin78@gmail.com>
2016-05-23 14:47:34 +0200

    ok pour la page_infos

--- page_infos.php
+++ page_infos.php
@@ -2,50 +2,50 @@
 require_once 'inc/header.php';
 
 ?>
-
 <div class="wrapper-infos">
-        <h1 id="title-infos" class="text-center"><i class="fa fa-quote-left" aria-hidden="true"></i>
-Informations sur notre restaurant<i class="fa fa-quote-right" aria-hidden="true"></i>
+        <h1 id="title-infos" class="text-center"><i class="fa fa-quote-left" aria-hidden="true"></i>Informations sur notre restaurant<i class="fa fa-quote-right" aria-hidden="true"></i>
 </h1>
     <div clas="info-cuisine">
-        <h2>Cuisine</h2>
-        <p>Française, Italienne, Américaine, Bar à vins</p>
+        <h2 class="title_cat">Cuisine</h2>
+        <p>Française, Italienne, Américaine, Bar à vins.</p>
     </div>
     <div class="info-repas">
-        <h2>Repas</h2>
-        <p> Déjeuner, Dîner, Boissons</p>
+        <h2 class="title_cat">Repas</h2>
+        <p> Déjeuner, Dîner, Boissons.</p>
     </div>
     <div class="info-carac">
-        <h2>Caractéristiques du restaurant</h2>
-        <p>Places assises, Service en salle, Accès personnes handicapées, Sert de l'alcool</p>
+        <h2 class="title_cat">Caractéristiques du restaurant</h2>
+        <p>Places assises, Service en salle, Accès personnes handicapées, Sert de l'alcool.</p>
     </div>
     <div class="info-horaires">
-        <h2>Horaires d'ouverture</h2>
-        <p>Lundi
+        <h2 class="title_cat">Horaires d'ouverture</h2>
+        <p><strong>Lundi : </strong>
             11:00 - 22:00<br>
-            Mardi
+            <strong>Mardi : </strong>
             11:00 - 22:00<br>
-            Mercredi
+            <strong>Mercredi : </strong>
             11:00 - 22:00<br>
-            Jeudi
+            <strong>Jeudi : </strong>
             11:00 - 22:00<br>
-            Vendredi
+            <strong>Vendredi : </strong>
             11:00 - 22:00<br>
-            Samedi
+            <strong>Samedi : </strong>
             11:00 - 22:00</p>
     </div>
     <div class="info-lieu">
-            <h2>Coordonnées et localisation</h2>
-            <p><br>Adresse : 32 rue Rosa Bonheur 33000 Bordeaux
-            <br>Lieu : Europe  >  France  >  Aquitaine  >  Gironde  >  Bordeaux
-            <br>Téléphone : 0 805 62 23 45</p>
+            <h2 class="title_cat">Coordonnées et localisation</h2>
+            <p><br><strong>Adresse : </strong> 32 rue Rosa Bonheur 33000 Bordeaux
+            <br><strong>Lieu : </strong> Europe    France  >  Aquitaine  >  Gironde  >  Bordeaux
+            <br><strong>Téléphone : </strong> 0 805 62 23 45</p>
     </div>
 
     <div class="icon-reco">
-        <h1>Recommandé par :</h1>
-        <p><a href="https://www.tripadvisor.fr/Restaurants"><i class="fa fa-tripadvisor" aria-hidden="true"></i>Tripadvisor</a></p>
-        <p><a href="http://www.lafourchette.com/ville/bordeaux/60566"><i class="fa fa-cutlery" aria-hidden="true"></i>La Fourchette</a></p>
-        <p><a href="http://www.yelp.fr/bordeaux"><i class="fa fa-yelp" aria-hidden="true"></i>Yelp</a></p>
+        <h2 class="title_cat">Recommandé par :</h2>
+        <ul class="reco_sites">
+        <li class="reco_site"><a href="https://www.tripadvisor.fr/Restaurants"><i class="fa fa-tripadvisor" aria-hidden="true"></i>Tripadvisor</a></li>
+        <li class="reco_site"><a href="http://www.lafourchette.com/ville/bordeaux/60566"><i class="fa fa-cutlery" aria-hidden="true"></i>La Fourchette</a></li>
+        <li class="reco_site"><a href="http://www.yelp.fr/bordeaux"><i class="fa fa-yelp" aria-hidden="true"></i>Yelp</a></li>
+        </ul>
     </div> 
 </div>
 
