d3c3983ce1ef12c9c4e8251ab6361b261d840243
Baptiste <baptistecousin78@gmail.com>
2016-05-23 14:29:52 +0200

    page_nfos

--- page_infos.php
+++ page_infos.php
@@ -4,44 +4,49 @@ require_once 'inc/header.php';
 ?>
 
 <div class="wrapper-infos">
-        <h1 id="title-infos" class="text-center">Informations sur notre restaurant</h1>
+        <h1 id="title-infos" class="text-center"><i class="fa fa-quote-left" aria-hidden="true"></i>
+Informations sur notre restaurant<i class="fa fa-quote-right" aria-hidden="true"></i>
+</h1>
     <div clas="info-cuisine">
         <h2>Cuisine</h2>
         <p>Française, Italienne, Américaine, Bar à vins</p>
     </div>
     <div class="info-repas">
-    <h2>Repas</h2>
-    <p> Déjeuner, Dîner, Boissons</p>
+        <h2>Repas</h2>
+        <p> Déjeuner, Dîner, Boissons</p>
     </div>
     <div class="info-carac">
-    <h2>Caractéristiques du restaurant</h2>
-    <p>Places assises, Service en salle, Accès personnes handicapées, Sert de l'alcool</p>
+        <h2>Caractéristiques du restaurant</h2>
+        <p>Places assises, Service en salle, Accès personnes handicapées, Sert de l'alcool</p>
     </div>
     <div class="info-horaires">
-    <h2>Horaires d'ouverture</h2>
-    <p>Lundi
-11:00 - 22:00<br>
-Mardi
-11:00 - 22:00<br>
-Mercredi
-11:00 - 22:00<br>
-Jeudi
-11:00 - 22:00<br>
-Vendredi
-11:00 - 22:00<br>
-Samedi
-11:00 - 22:00</p>
+        <h2>Horaires d'ouverture</h2>
+        <p>Lundi
+            11:00 - 22:00<br>
+            Mardi
+            11:00 - 22:00<br>
+            Mercredi
+            11:00 - 22:00<br>
+            Jeudi
+            11:00 - 22:00<br>
+            Vendredi
+            11:00 - 22:00<br>
+            Samedi
+            11:00 - 22:00</p>
     </div>
     <div class="info-lieu">
-    <h2>Coordonnées et localisation</h2>
-    <p><br>Adresse : 32 rue Rosa Bonheur 33000 Bordeaux
-    <br>Lieu : Europe  >  France  >  Aquitaine  >  Gironde  >  Bordeaux
-    <br>Téléphone : 0 805 62 23 45</p>
+            <h2>Coordonnées et localisation</h2>
+            <p><br>Adresse : 32 rue Rosa Bonheur 33000 Bordeaux
+            <br>Lieu : Europe  >  France  >  Aquitaine  >  Gironde  >  Bordeaux
+            <br>Téléphone : 0 805 62 23 45</p>
     </div>
 
-    <div class="reco">
-<h1>Recommandé par :</h1>
-</div> 
+    <div class="icon-reco">
+        <h1>Recommandé par :</h1>
+        <p><a href="https://www.tripadvisor.fr/Restaurants"><i class="fa fa-tripadvisor" aria-hidden="true"></i>Tripadvisor</a></p>
+        <p><a href="http://www.lafourchette.com/ville/bordeaux/60566"><i class="fa fa-cutlery" aria-hidden="true"></i>La Fourchette</a></p>
+        <p><a href="http://www.yelp.fr/bordeaux"><i class="fa fa-yelp" aria-hidden="true"></i>Yelp</a></p>
+    </div> 
 </div>
 
    
