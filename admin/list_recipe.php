9ccaa9382d8a34cf0a9fb94d5efbb9f709bb65f2
Millaa Millaa <contact.myriambugnazet@gmail.com>
2016-05-23 14:32:09 +0200

    2 eme jonction sql
    !!!!! plus de points plus de points

--- admin/list_recipe.php
+++ admin/list_recipe.php
@@ -8,13 +8,22 @@ $allRecipes = array_merge(selectCategory('entrance'), selectCategory('dish'), se
 
 include_once 'inc/header.php';
 
+
 foreach($allRecipes as $plat => $recettes){
+	$res = $db->prepare('SELECT recipes.id, recipes.title, recipes.date_publish, recipes.link,  recipes.content, users.id, users.firstname, users.lastname 
+	FROM recipes INNER JOIN users ON recipes.author_id = users.id WHERE recipes.id = :id');
+	$res->bindParam(':id', $recettes['id'], PDO::PARAM_INT);
+	if($res->execute()){
+		// $recipe contient mon recipe extrait de la bdd
+		$recipe = $res->fetch(PDO::FETCH_ASSOC);	
+	}
+
 	echo '<div class="row">';
 		echo '<h3 class="title-list">'.$recettes['title'].'</h3><br>';
 		echo '<img class="img-list col-sm-2" src="'.$recettes['link'].'">';
 		echo '<div class="col-sm-6">';
 			echo cutString($recettes['content'],0, 220, ' [...]').'<br><br>';
-			echo '<p class="text-right"><strong>Ajouté le : </strong>'.$recettes['date_publish']."<strong> par : </strong><em>".$recettes['author_id'].'</em></p>';
+			echo '<p class="text-right"><strong>Ajouté le : </strong>'.$recettes['date_publish']."<strong> par : </strong><em>".$recipe['firstname']. ' ' .$recipe['lastname'].'</em></p>';		
 		echo '</div>';
 		echo '<div class="wrapper-buttons col-sm-4">';
 			echo '<a href="update_article.php?id='.$recettes['id'].'"><span class="glyphicon glyphicon-wrench"></span> Modifier</a><br>';
