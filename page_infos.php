571601d28db94ab167c1d61a2dbb297b70e1431b
Baptiste <baptistecousin78@gmail.com>
2016-05-23 14:16:09 +0200

    page infos

--- page_infos.php
+++ page_infos.php
@@ -1,1 +1,41 @@
-<div 
+<div class="wrapper-infos">
+        <h1 id="title-infos" class="text-center">Informations sur notre restaurant</h1>
+    <div clas="info-cuisine">
+        <h2>Cuisine</h2>
+        <p>Française, Italienne, Américaine, Bar à vins</p>
+    </div>
+    <div class="info-repas">
+    <h2>Repas</h2>
+    <p> Déjeuner, Dîner, Boissons</p>
+    </div>
+    <div class="info-carac">
+    <h2>Caractéristiques du restaurant</h2>
+    <p>Places assises, Service en salle, Accès personnes handicapées, Sert de l'alcool</p>
+    </div>
+    <div class="info-horaires">
+    <h2>Horaires d'ouverture</h2>
+    <p>Lundi
+11:00 - 22:00<br>
+Mardi
+11:00 - 22:00<br>
+Mercredi
+11:00 - 22:00<br>
+Jeudi
+11:00 - 22:00<br>
+Vendredi
+11:00 - 22:00<br>
+Samedi
+11:00 - 22:00</p>
+    </div>
+    <div class="info-lieu">
+    <h2>Coordonnées et localisation</h2>
+    <p><br>Adresse : 32 rue Rosa Bonheur 33000 Bordeaux
+    <br>Lieu : Europe  >  France  >  Aquitaine  >  Gironde  >  Bordeaux
+    <br>Téléphone : 0 805 62 23 45</p>
+    </div>
+</div>
+
+<div class="reco">
+<h1>Recommander par :</h1>
+<
+</div>    
