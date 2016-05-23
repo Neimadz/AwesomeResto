e412e5b4bc6c757a6b453bdc2acf6121afd88439
Millaa Millaa <contact.myriambugnazet@gmail.com>
2016-05-23 09:27:49 +0200

    search le titre mène vers une recette

--- inc/functions.php
+++ inc/functions.php
@@ -15,7 +15,7 @@ function showArticles($art) {
 
 function showSearchResult($art, $key) {
     echo '<article class="search-result">';
-	echo '<h3 class="search-recipe-title">' . str_ireplace($key, '<span class="search-recipe-keyword">' . $key . '</span>', $art['title']) . '</h3>';
+	echo '<h3 class="search-recipe-title"><a href="http://'.$_SERVER['HTTP_HOST'].'/AwesomeResto/read_recipe.php?id='.$art['id'].'">' . str_ireplace($key, '<span class="search-recipe-keyword">' . $key . '</span>', $art['title']) . '</a></h3>';
 	echo '<p class="search-recipe-date">Publié le <b>' . date('d/m/Y', strtotime($art['date_publish'])) . '</b></p>';
 	echo '<img class="search-recipe-img" src="' . $art['link'] . '">';
 	echo '<p class="search-recipe-content">' . str_ireplace($key, ' <span class="search-recipe-keyword"> ' . $key . ' </span> ', $art['content']) . '</p>';
