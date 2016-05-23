88c305c0da4924323a59faa0c86d7c4c10587adc
Millaa Millaa <contact.myriambugnazet@gmail.com>
2016-05-23 12:06:04 +0200

    VERSION INNER JOIN SQL

--- read_recipe.php
+++ read_recipe.php
@@ -7,7 +7,7 @@ require_once 'inc/connect.php';
 /* On vérifie que l'ID de la recette existe et n'est pas vide
 Si l'id n'est pas de type numérique, on force la valeur à 1
 */
-if(isset($_GET['id']) && !empty($_GET['id'])){
+/*if(isset($_GET['id']) && !empty($_GET['id'])){
 	$idRecipe = $_GET['id']; 
 	if(!is_numeric($idRecipe)){
 		$idRecipe = 1;
@@ -23,6 +23,26 @@ if(!empty($_GET)){
 
 	// $recipe contient mon recipe extrait de la bdd
 	$recipe = $res->fetch(PDO::FETCH_ASSOC);
+*/
+
+if(isset($_GET['id']) && !empty($_GET['id'])){
+	$userRecipes = $_GET['id']; 
+	if(!is_numeric($userRecipes)){
+		$userRecipes = 1;
+	}
+}
+
+if(!empty($_GET)){
+	if(isset($userRecipes)){
+	$res = $db->prepare('SELECT * FROM recipes INNER JOIN users WHERE recipes.id = users.id and users.id = :id ');
+	$res->bindParam(':id', $userRecipes, PDO::PARAM_INT);
+	if($res->execute()){
+		// $recipe contient mon join extrait de la bdd
+		$join = $res->fetch(PDO::FETCH_ASSOC);
+	}
+
+	
+	
 
 	} else {
 		echo 'Article introuvable !';
@@ -32,19 +52,20 @@ require_once 'inc/header.php'
 ?>
 	<div class="container">
 		<div class="row">
-			<div class="imgOneRecipe text-center">
+			<div class="imgOnejoin text-center">
 				<br>
-					<h2 class="readTitle"><?php echo $recipe['title'];?></h2>
+					<h2 class="readTitle"><?php echo $join['title'];?></h2>
+				 	<h2 class="readTitle"> Publié par <?php echo $join['firstname'];?></h2>
 				<br>
-					<p class="readPublish">Publié le :<?php echo $recipe['date_publish'];?></p>
+					<p class="readPublish"> Disponible depuis le :<?php echo $join['date_publish'];?></p>
 				<br>
 					<div class="col-xs-12 col-sm-6 col-sm-offset-3">
 						<div class="thumbnail center-block">
-							<div class="index-recipe-img-container text-center">
-						    	<img class="index-recipe-img" src="<?php echo $recipe['link'];?>">
-						    	<div class="index-recipe-ingredients"><?php echo $recipe['ingredients'];?></div>
+							<div class="index-join-img-container text-center">
+						    	<img class="index-join-img" src="<?php echo $join['link'];?>">
+						    	<div class="index-join-ingredients"><?php echo $join['ingredients'];?></div>
 					    	</div>
-							<?php echo $recipe['content'];?>
+							<?php echo $join['content'];?>
 					    </div>
 				  	</div>
 			</div>
